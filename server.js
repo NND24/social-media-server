bdb53ada01955397197d4e714ecce6ccc9e2b828
Dat Nguyen <=>
2024-11-01 17:29:39 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+require("dotenv").config();
+require("express-async-errors");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
